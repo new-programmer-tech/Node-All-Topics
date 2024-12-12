c63219ee386b98f511f475622eff623ff2dc07f3
Harshal.Ogale-at-447515355797 <harshal.ogale@flentas.com>
2024-12-13 01:21:15 +0530

    mongodb

--- Mini-Project/index.js
+++ Mini-Project/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port =3000
 //by this we can use our forms
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname,'public')));
 
 app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res) => {
   res.render("index")
